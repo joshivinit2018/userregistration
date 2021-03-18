da814eccbd164ca66a404b969b5525630c9a7271
joshivinit2018 <joshivinit2018@gmail.com>
2021-03-18 16:49:50 +0530

    [vinit] : add check phone number is valid or not

--- Demo.java
+++ Demo.java
@@ -2,12 +2,11 @@ import java.util.regex.*;
 
 public class Demo {
    public static void main(String args[]) {
-      String email = "abc.xyz@bl.co.in";
-      String regex = ("^[a-z]{1,}[.]([a-z]*)[@][a-z]{1,}[.]"
-											+ "[a-z]{1,}[.]([a-z]*)");
+      String phone = "91 7588111280";
+      String regex = ("^[0-9]{2}\\s[0-9]{10}$");
       Pattern p = Pattern.compile(regex);
-      Matcher m = p.matcher(email);
-      System.out.println("email is: " + email);
-      System.out.println("Is the fname is valid? " + email.matches(regex));
+      Matcher m = p.matcher(phone);
+      System.out.println("email is: " + phone);
+      System.out.println("Is the fname is valid? " + phone.matches(regex));
    }
 }
