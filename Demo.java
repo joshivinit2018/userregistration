92def62754841751bea95ffb46458b3b68879bf5
joshivinit2018 <joshivinit2018@gmail.com>
2021-03-18 16:59:57 +0530

    [vinit] : add check second rule for pwd is valid or not

--- Demo.java
+++ Demo.java
@@ -2,11 +2,11 @@ import java.util.regex.*;
 
 public class Demo {
    public static void main(String args[]) {
-      String pwd1 = "Vintax77";
-      String regex = ("[a-zA-Z0-9]{8,}");
+      String pwd2 = "Vintax77";
+      String regex = ("(?=.*[A-Z])[a-zA-Z0-9]{8,}");
       Pattern p = Pattern.compile(regex);
-      Matcher m = p.matcher(pwd1);
-      System.out.println("pwassward 1 rule is is: " + pwd1);
-      System.out.println("Is the fname is valid? " + pwd1.matches(regex));
+      Matcher m = p.matcher(pwd2);
+      System.out.println("pwasward 1 rule is is: " + pwd2);
+      System.out.println("Is the fname is valid? " + pwd2.matches(regex));
    }
 }
