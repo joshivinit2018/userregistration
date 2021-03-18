0059936181815dc0af7bbff6d34952f0bd88279d
joshivinit2018 <joshivinit2018@gmail.com>
2021-03-18 06:13:02 +0530

    [vinit] : add check Lastname is valid or not

--- Demo.java
+++ Demo.java
@@ -2,11 +2,11 @@ import java.util.regex.*;
 
 public class Demo {
    public static void main(String args[]) {
-      String fname = "Vinit";
+      String lname = "Joshi";
       String regex = "^[A-Z]{1}[a-z]{3,}$";
       Pattern p = Pattern.compile(regex);
-      Matcher m = p.matcher(fname);
-      System.out.println("first name is: " + fname);
-      System.out.println("Is the fname is valid? " + fname.matches(regex));
+      Matcher m = p.matcher(lname);
+      System.out.println("last name is: " + lname);
+      System.out.println("Is the fname is valid? " + lname.matches(regex));
    }
 }
